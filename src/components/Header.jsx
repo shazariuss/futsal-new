e69ef0434872df8a8b7c9a4a4295e0b1d59e96f4
shazariuss <shazariuss@gmail.com>
2025-07-14 22:34:25 +0500

    logo and title changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,16 +61,16 @@ const Header = () => {
                         {/* Logo and Title */}
                         <Link to="/">
                             <div className="flex items-center space-x-4">
-                                <div className="w-16 h-16 bg-white rounded-lg flex items-center justify-center">
+                                <div className="w-16 h-16 bg-transparent rounded-lg flex items-center justify-center">
                                     <img
-                                        src="https://upload.wikimedia.org/wikipedia/commons/4/47/Uzbekistan_Football_Federation.svg"
+                                        src="/logo.png"
                                         alt="UZLLF Logo"
-                                        className="w-12 h-12"
+                                        className="w-18"
                                     />
                                 </div>
                                 <div>
                                     <h1 className="text-xl font-bold text-white">
-                                        UZBEKISTAN FOOTBALL LEAGUE FEDERATION
+                                        LLF Uzbekistan
                                     </h1>
                                     <p className="text-sm text-purple-200">
                                         UZLLF
